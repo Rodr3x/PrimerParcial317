c0fc7cc13055a9c36778f4e5d9a88b53e4dcde32
Sergio Rodrigo Paye Yujra <74877481+Rodr3x@users.noreply.github.com>
2024-05-20 15:51:48 -0400

    Add files via upload

---
+++ ejercicio11/Calculo/Infija.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo
+{
+    public class Infija
+    {
+        public static int Evaluar(string postfija)
+        {
+            Stack<int> pila = new Stack<int>();
+
+            foreach (string token in postfija.Split(' '))
+            {
+                //MessageBox.Show(token);
+                if (int.TryParse(token, out int numero))
+                {
+                    pila.Push(numero);
+                }
+                else
+                {
+                    if (pila.Count < 2)
+                    {
+                        throw new InvalidOperationException("Expresión mal formada.");
+                    }
+
+                    int b = pila.Pop();
+                    int a = pila.Pop();
+
+                    switch (token)
+                    {
+                        case "+": pila.Push(a + b); break;
+                        case "-": pila.Push(a - b); break;
+                        case "*": pila.Push(a * b); break;
+                        case "/":
+                            if (b == 0)
+                                throw new DivideByZeroException("División por cero no permitida.");
+                            pila.Push(a / b);
+                            break;
+                        default:
+                            throw new ArgumentException($"Operador desconocido: {token}");
+                    }
+                }
+            }
+
+            if (pila.Count != 1)
+            {
+                throw new InvalidOperationException("La expresión no se pudo evaluar correctamente.");
+            }
+
+            return pila.Pop();
+        }
+    }
+}
---
+++ ejercicio11/WinFormsApp1/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using Calculo;
+
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button boton = sender as Button;
+            if (boton != null)
+            {
+                
+                if (resultado == 0 && char.IsDigit(boton.Text, 0))
+                    textBox1.Clear();
+
+                textBox1.Text += boton.Text + " ";
+            }
+        }
+
+        public int resultado = 2;
+        public Boolean flag = false;
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string expresionPostfija = textBox1.Text;
+                expresionPostfija = expresionPostfija.Trim();
+
+                //MessageBox.Show(expresionPostfija);
+                int resultado = Infija.Evaluar(expresionPostfija);
+                textBox1.Text = resultado.ToString()+" ";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en la expresión: " + ex.Message);
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+}
